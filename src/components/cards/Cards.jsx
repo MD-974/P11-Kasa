3177abc87a49d0a0d2993a0a6b8f73f8f0929210
MD-974 <davidcolmoulin974@gmail.com>
2024-08-15 15:56:01 +0400

    mise en place des images dans Cards et ajout du scss pour les cards

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -1,16 +1,40 @@
 import './cards.scss';
-function Cards() {
-        const articles = Array(20).fill(null).map((_, index) => (
-        <article key={index} className='cards__article'>
-            <p className='cards__article--title'>Appartement cosy</p>
-        </article>
-    ));
+import logements from '../../../public/logements.json';
+// function Cards() {
 
+//         const articles = Array(20).fill(null).map((_, index) => (
+//         <article key={index} className='cards__article'>
+//             <p className='cards__article--title'>{logement.title}</p>
+//         </article>
+//     ));
+
+//     return (
+//         <div className='cards'>
+//             {articles}
+//         </div>
+//     )
+// }
+
+// export default Cards
+
+
+function Cards () {
     return (
         <div className='cards'>
-            {articles}
+            {logements.map((logement) => (
+                <article key={logement.id} className='cards__article'>
+                    <div className='cards__image-container'>
+                        <img 
+                            className='cards__image' 
+                            src={logement.cover} 
+                            alt={logement.title} 
+                        />
+                        <p className='cards__title'>{logement.title}</p>
+                    </div>
+                </article>
+            ))}
         </div>
-    )
+    );
 }
 
 export default Cards
