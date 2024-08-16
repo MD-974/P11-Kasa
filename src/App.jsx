fa75311814a4da7423dea4420a16cc3bb201979b
MD-974 <davidcolmoulin974@gmail.com>
2024-08-16 20:30:31 +0400

    🔧 Creation du dossier logement et ajout de la route dans App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-
 import Footer from './components/footer/Footer'
 import Header from './components/header/Header'
 
 import Home from "./pages/home/Home";
 import About from "./pages/about/About";
+import Logement from "./pages/logement/Logement";
 import Error from "./pages/error/Error";
+
 import './app.scss'
 
 
@@ -17,6 +18,7 @@ function App() {
       <Routes>
         <Route path="/" Component={Home} />
         <Route path="/about" Component={About} />
+        <Route path="/logement" Component={Logement} />
         <Route path="*" Component={Error} />
       </Routes>
       <Footer />
---
+++ src/pages/logement/Logement.jsx
@@ -0,0 +1,10 @@
+
+function Logement () {
+    return (
+        <main className="main">
+            salut cest bien le logement
+        </main>
+    )
+}
+
+export default Logement
