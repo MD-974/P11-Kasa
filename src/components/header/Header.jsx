6c2f036865b5c6eb1af0a694023854c3221bf619
MD-974 <davidcolmoulin974@gmail.com>
2024-08-27 12:23:58 +0400

    🔨 Mise en place media querie pour la page accueil

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,15 +1,15 @@
-import Red from "../logoKasa/logoRed"
+// import Red from "../logoKasa/logoRed"
 import Nav from "../nav/nav"
 
-import './header.scss'
+import "./header.scss"
 
-function Header () {
-    return (
-        <header className="header">
-            <Red />
-            <Nav />
-        </header>
-    )
+function Header() {
+  return (
+    <header className="header">
+      <img src="./src/assets/logo-red.svg" className="logo__red" alt="" />
+      <Nav />
+    </header>
+  )
 }
 
 export default Header
