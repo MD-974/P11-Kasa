fadd06a2307743008859f052ae113312bc70559a
MD-974 <davidcolmoulin974@gmail.com>
2024-09-16 21:23:15 +0400

    ✅ Carousel.jsx  fonction d affichage avec une image ou plus dans le carrousel et si une image suppression des fleches et du compteur.

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -27,38 +27,45 @@ function Carousel({ images }) {
         src={images[currentIndex]}
         alt={`Slide ${currentIndex + 1}`}
       />
-      <div className="carousel__navigation">
-        <svg
-          className="carousel__prev"
-          onClick={handlePrev}
-          xmlns="http://www.w3.org/2000/svg"
-          height="80"
-          width="47"
-          viewBox="0 0 320 512"
-        >
-          <path
-            fill="white"
-            d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"
-          />
-        </svg>
 
-        <svg
-          className="carousel__next"
-          onClick={handleNext}
-          xmlns="http://www.w3.org/2000/svg"
-          height="80"
-          width="47"
-          viewBox="0 0 320 512"
-        >
-          <path
-            fill="white"
-            d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"
-          />
-        </svg>
-      </div>
-      <div className="carousel__counter">
-        {currentIndex + 1}/{images.length}
-      </div>
+      {/* Afficher les fleches de navigation et le compteur 
+      seulement si plus d'une image dans le carousel */}
+      {images.length > 1 && (
+        <>
+          <div className="carousel__navigation">
+            <svg
+              className="carousel__prev"
+              onClick={handlePrev}
+              xmlns="http://www.w3.org/2000/svg"
+              height="80"
+              width="47"
+              viewBox="0 0 320 512"
+            >
+              <path
+                fill="white"
+                d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"
+              />
+            </svg>
+
+            <svg
+              className="carousel__next"
+              onClick={handleNext}
+              xmlns="http://www.w3.org/2000/svg"
+              height="80"
+              width="47"
+              viewBox="0 0 320 512"
+            >
+              <path
+                fill="white"
+                d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"
+              />
+            </svg>
+          </div>
+          <div className="carousel__counter">
+            {currentIndex + 1}/{images.length}
+          </div>
+        </>
+      )}
     </div>
   )
 }
